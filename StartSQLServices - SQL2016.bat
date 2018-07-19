8faa375301730e2cfbb0fcd72993d738bc1a7af2
hnasiri <hamid.nasiri@hotmail.com>
2018-07-19 23:05:54 +0430

    1'st commit

---
+++ StartSQLServices - SQL2016.bat
@@ -0,0 +1,11 @@
+# Stop SQL server services.
+net stop MSSQL$TEW_SQLEXPRESS
+net start MSSQL$SQL2016
+#net start MSSQLFDLauncher$SQL2016
+net start SQLAgent$SQL2016
+#net start MSOLAP$SQL2016
+net start SQLBrowser
+net start SSISTELEMETRY130
+net start MsDtsServer130
+#net start ReportServer$SQL2016
+net start SQLWriter
---
+++ StopSQLServices_SQL2016.bat
@@ -0,0 +1,9 @@
+net stop SQLAgent$SQL2016
+#net stop MSOLAP$SQL2016
+net stop MsDtsServer130
+net stop SSISTELEMETRY130
+#net stop ReportServer$SQL2016
+#net stop MSSQLFDLauncher$SQL2016
+net stop MSSQL$SQL2016
+net stop SQLWriter
+net stop SQLBrowser
